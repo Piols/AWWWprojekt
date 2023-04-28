ffb67af2d53ed749a9febde29a318e6e52a32337
Piotr Kępczyński <pk429304@khaki03.mimuw.edu.pl>
2023-04-28 17:17:53 +0200

    Poprawki i uwagi z labu

--- compiler/mainpage/models.py
+++ compiler/mainpage/models.py
@@ -1,26 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-from django.db import models
-
-class Folder(models.Model):
+class FolderOrFile(models.Model):
 	name = models.CharField(max_length=255)
 	description = models.TextField(blank=True)
 	created_at = models.DateTimeField(auto_now_add=True)
-	owner = models.ForeignKey('User', on_delete=models.CASCADE)
+	owner = models.ForeignKey(User, on_delete=models.CASCADE)
 	is_available = models.BooleanField(default=True)
 	availability_changed_at = models.DateTimeField(auto_now=True)
 	last_content_change_at = models.DateTimeField(auto_now=True)
-	parent_folder = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL)
+	# W templacie {% for sub_file in file.subfolders.all %}
+	# Oddzielny template od wypisywania folderow, ktory potrafi sie rekurencyjnie wywolywac.
 
-class File(models.Model):
-	name = models.CharField(max_length=255)
-	description = models.TextField(blank=True)
-	created_at = models.DateTimeField(auto_now_add=True)
-	owner = models.ForeignKey('User', on_delete=models.CASCADE)
-	is_available = models.BooleanField(default=True)
-	availability_changed_at = models.DateTimeField(auto_now=True)
-	last_content_change_at = models.DateTimeField(auto_now=True)
-	parent_folder = models.ForeignKey('Folder', null=True, on_delete=models.SET_NULL)
+	class Meta:
+		abstract = true
+
+class Folder(FolderOrFile):
+	parent_folder = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name = 'subfolders')
+
+
+class File(FolderOrFile):
+	content = models.TextField()
+	parent_folder = models.ForeignKey(Folder, null=True, blank=True, on_delete=models.SET_NULL, related_name = 'subfiles')
 
 class FileSection(models.Model):
 	name = models.CharField(max_length=255, blank=True, null=True)
@@ -30,19 +31,20 @@ class FileSection(models.Model):
 	end_line_number = models.PositiveIntegerField()
 	start_char_number = models.PositiveIntegerField(null=True, blank=True)
 	end_char_number = models.PositiveIntegerField(null=True, blank=True)
-	section_type = models.ForeignKey('SectionType', on_delete=models.CASCADE)
-	status = models.ForeignKey('SectionStatus', on_delete=models.CASCADE)
+	SECTION_TYPES = [
+		("PR", "Procedure"),
+		("CO", "Comment"),
+		("CD", "Compiler directive"),
+		("VD", "Variable declaration"),
+		("IA", "Inline assembly"),
+	]
+	section_type = models.CharField(max_length=2, choices=SECTION_TYPES)
+	SECTIONS_STATUSES = [
+		("C", "Correct"),
+		("W", "Warning"),
+		("E", "Error"),
+	]
+	status = models.CharField(max_length=1, choices=SECTIONS_STATUSES)
 	status_data = models.TextField(blank=True)
 	content = models.TextField()
-	origin_file = models.ForeignKey('File', on_delete=models.CASCADE)
-
-class SectionType(models.Model):
-	name = models.CharField(max_length=255)
-
-class SectionStatus(models.Model):
-	name = models.CharField(max_length=255)
-
-class User(models.Model):
-	name = models.CharField(max_length=255)
-	login = models.CharField(max_length=255, unique=True)
-	password = models.CharField(max_length=255)
+	origin_file = models.ForeignKey(File, on_delete=models.CASCADE)
--- compiler/mainpage/views.py
+++ compiler/mainpage/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
+# Edycja plikow i podzial na sekcje nie potrzebna. Potrzeba umiec skompilowac plik
+
 def mainpage(request):
+	# Mozna odzyskac aktualnego uzytkownika
 	folders = Folder.objects.order_by('-name')
 	files = File.objects.order_by('-name')
 	
